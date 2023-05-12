87c6ed23c386559681e7c0d0b62778b15dae943a
Daniel Maina <daniel.maina@thejitu.com>
2023-05-12 10:55:20 +0300

    added the dockerfile

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.send("<h2>Hi Daniel</h2>")
+})
+
 app.listen(port, () => {
     console.log(`the server is running in port ${port}`)
 })
