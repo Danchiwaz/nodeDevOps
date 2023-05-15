bbf7f9d5edf958413fbd3e761c11fdbe041c39a0
Daniel Maina <daniel.maina@thejitu.com>
2023-05-15 11:46:20 +0300

    added the Dokcerfile and .dockerignore file

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM node 
+FROM node:alpine 
 WORKDIR /app
 COPY package.json . 
 RUN npm install 
-COPY . ./ 
-EXPOSE 3000 
-CMD ["nodemon", "index.js"]
+COPY . ./
+EXPOSE 5000 
+CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -10,9 +10,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hi Daniel</h2>")
+    res.send("<h2>Hi Daniel Maina testing the docker file</h2>")
 })
 
 app.listen(port, () => {
     console.log(`the server is running in port ${port}`)
 })
+
+
+// volumes ensures we have persistent data in our containers 
