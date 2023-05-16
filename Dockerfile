a45d6d4d9c453bf398f0035317d5a52caea778d3
Daniel Maina <daniel.maina@thejitu.com>
2023-05-16 14:31:00 +0300

    added the environment variables

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM node:alpine 
 WORKDIR /app
-COPY package.json . 
-RUN npm install 
+COPY package*.json . 
+RUN npm ci 
+ARG NODE_ENV
+RUN if [ "$NODE_ENV" = "development" ]; \
+        then npm install; \ 
+        else npm install --only=production; \
+        fi
 COPY . ./
-EXPOSE 5000 
-CMD ["npm", "run", "dev"]
+ENV PORT 5000
+EXPOSE $PORT
+CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send(`<h2>checking if the infrastructure is valid</h2>`)
+    res.send(`<h2>this works shenanigans dfsdfbhgug</h2>`)
 })
 
 app.listen(port, () => {
@@ -27,4 +27,24 @@ app.listen(port, () => {
 // creating a volume override the folder in the container, solution creating an anonymous volume 
 
 // example
-// docker run -d -v ${pwd}:/app -v /app/node_modules -p 5000:5000 --name node-app node-app-image 
+// docker run -d -v ${pwd}:/app:ro -v /app/node_modules -p 5000:5000 --name node-app node-app-image 
+// execute in interactive mode 
+// docker exec -it container-id //bin//sh
+
+// using environmanet variable 
+// ===========================================
+// docker run -d -v ${pwd}:/app:ro -v /app/node_modules --env PORT=4000 -p 5000:4000 --name node-app node-app-image
+
+
+
+// check if the env is set 
+// =====================================
+// docker exec -it node-app //bin//sh 
+// printenv 
+
+// using the .env file 
+// docker run -d -v ${pwd}:/app:ro -v /app/node_modules --env-file ./.env -p 5000:5000 --name node-app node-app-image
+
+// remove unneccessary volumes 
+// docker volume prune 
+// docker rm container_name -fv  == delete the volume associated with that container 
